271302d7cfeb260c60e3e2aa1b1af0dae126b8ce
Prachit Pandit <panditprachit20@gmail.com>
2023-11-28 16:55:29 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-export default {
-  base: "/3d-portfolio/",
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 5000, // set your desired limit in kilobytes
+    chunkSizeWarningLimit: 25000, // set your desired limit in kilobytes
   },
-};
+});
